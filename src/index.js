8c1b0c93e6d4c428e082b1250d917657db775f2b
Samuel Chávez <samuelchvez@gmail.com>
2017-05-17 17:50:18 -0600

    Exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 export * as types from './types';
 export * as actions from './actions';
 export * as reducers from './reducers';
-
-const REDUX_JWT = { types, actions, reducers };
-
-export default REDUX_JWT;
